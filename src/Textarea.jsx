4c5ec013accc7f0c306fd4adba73ba534e380248
Viacheslav Saprykin <saprykinvp@gmail.com>
2025-05-09 17:03:17 +0300

    modified: TextArea; add Warning

--- src/Textarea.jsx
+++ src/Textarea.jsx
@@ -1,5 +1,29 @@
+import { useState } from 'react';
+import TheWarning from './TheWarning';
+
 export default function Textarea() {
+  const [text, setText] = useState('');
+  const [showWarning, setShowWarning] = useState(false);
+
+  const handleChange = (e) => {
+    let newText = e.target.value;
+
+    if (newText.includes('<script>')) {
+      setShowWarning(true);
+      newText = newText.replace('<script>', '');
+    }
+    setText(newText);
+  };
+
   return (
-    <textarea className="textarea"/>
-  )
+    <div className="textarea">
+      <textarea
+        value={text}
+        onChange={handleChange}
+        placeholder="Enter your text here..."
+        spellCheck="false"
+      />
+      {showWarning ? <TheWarning /> : null}
+    </div>
+  );
 }
---
+++ src/TheWarning.jsx
@@ -0,0 +1,5 @@
+export default function TheWarning() {
+  return (
+    <p className="warning">No script tag allowed!</p>
+  )
+}
