4ef924bacd99886434d928d65475333a0ad7aed9
Viacheslav Saprykin <saprykinvp@gmail.com>
2025-05-12 18:31:39 +0300

    fixed: folder structure

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import Stats from './Stats';
 import Textarea from './Textarea';
-import { FACEBOOK_MAX_CHARACTERS, INSTAGRAM_MAX_CHARACTERS } from './constants';
+import { FACEBOOK_MAX_CHARACTERS, INSTAGRAM_MAX_CHARACTERS } from '../lib/allConstants'
 
 export default function Container() {
   const [text, setText] = useState('');
